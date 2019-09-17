4f457bad330fa35a3e84da89c6e8bc49a02967b2
Nikita Uchitelev <uchitelev.nikita@gmail.com>
2019-09-17 12:52:21 +0300

    Update AerospikeConnection.scala
    убрал таймауты на которых падает сборка

--- src/main/scala/com/aerospike/spark/sql/AerospikeConnection.scala
+++ src/main/scala/com/aerospike/spark/sql/AerospikeConnection.scala
@@ -77,11 +77,11 @@ object AerospikeConnection extends LazyLogging {
     val newClient = new AerospikeClient(clientPolicy, host, port)
 
     // set all the timeouts
-    newClient.writePolicyDefault.timeout = timeOut
-    newClient.readPolicyDefault.timeout = timeOut
-    newClient.scanPolicyDefault.timeout = timeOut
+    //newClient.writePolicyDefault.timeout = timeOut
+    //newClient.readPolicyDefault.timeout = timeOut
+    //newClient.scanPolicyDefault.timeout = timeOut
     newClient.scanPolicyDefault.socketTimeout = socketTimeOut
-    newClient.queryPolicyDefault.timeout = timeOut
+    //newClient.queryPolicyDefault.timeout = timeOut
  
     for (node <- newClient.getNodes) {
       clientCache += (node.getHost.toString -> newClient)
